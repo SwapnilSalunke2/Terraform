f820c29803f1cf38c4bbf8b245e4d740a2a6b3dc
Swapnil <106924058+SwapnilSalunke2@users.noreply.github.com>
2024-02-14 19:00:42 +0530

    Update sample.tf

--- sample.tf
+++ sample.tf
@@ -1,6 +1,6 @@
 
 provider "aws" {
-  region = "eu-north-1b"  
+  region = "eu-north-1"  
 }
 
 resource "aws_instance" "my-instance" 
@@ -8,7 +8,7 @@ resource "aws_instance" "my-instance"
   ami           = "ami-0014ce3e52359afbd" 
   instance_type = "t3.micro"
   key_name      = "stockholm-key"
-  security_group_id = ["sg-07fa648b05aa14fcc"] 
+  security_group_id = ["sg-0d53160382ceee98e"] 
 
   tags = 
   {
