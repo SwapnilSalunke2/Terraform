27a5442619ba5beb1e876a4bed6e216eab9e32c7
Swapnil <106924058+SwapnilSalunke2@users.noreply.github.com>
2024-02-14 19:21:40 +0530

    Update s2.tf

--- s2.tf
+++ s2.tf
@@ -6,5 +6,5 @@ resource "aws_instance" "my-instance" {
   ami           = "ami-0014ce3e52359afbd"         
   instance_type = "t3.micro"             
   key_name      = "stockholm-key"
-  security_groups = ["sg-07fa648b05aa14fcc"]
+  security_groups = ["sg-0d53160382ceee98e"]
 }
